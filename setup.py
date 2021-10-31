f93fbe2ae89939f4679484519069f45613ff0856
ksproska <kamila.sproska@gmail.com>
2021-10-31 17:29:19 +0100

    long description markdown for ```python ...```

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     author_email="kamila.sproska@gmail.com",
     description="Class for translating texts and detecting language (based on Google Translator).",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords=['python', 'google', 'translator', 'simple', 'google translator'],
     url="https://github.com/ksproska/google_translator_simplified",
     packages=setuptools.find_packages()
