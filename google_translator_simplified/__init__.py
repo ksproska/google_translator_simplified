5e2c3ffd8e402bddfe3a55007e8bfd35408e43f6
ksproska <kamila.sproska@gmail.com>
2021-10-31 17:45:45 +0100

    v7

--- google_translator_simplified/__init__.py
+++ google_translator_simplified/__init__.py
@@ -2,7 +2,7 @@
 # author ksproska
 # version : 0.0.1
 __all__ = 'google_translator_simplified'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 from .translator_complex import GoogleTranslator
 from .translator_constant import DEFAULT_SERVICE_URLS, LANGUAGES
